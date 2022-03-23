a2a909eea69f27d752ad7c08b0050cb4a1542ccc
Shan Padayhag <shanpadayhag@gmail.com>
2022-03-23 11:29:01 +0800

    CARD-3 Added scholarship table

---
+++ database/migrations/2022_03_23_031515_create_finance_system_scholarship_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFinanceSystemScholarshipTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('finance_system_scholarship', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('type');
+            $table->boolean('is_active');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('finance_system_scholarship');
+    }
+}
