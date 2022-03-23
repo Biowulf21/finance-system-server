2407d7b586c68240a9a9a548e5e999b03e6d542c
Shan Padayhag <shanpadayhag@gmail.com>
2022-03-23 14:10:10 +0800

    Fixed migration

--- database/migrations/2022_03_23_051455_create_finance_system_transaction_table.php
+++ database/migrations/2022_03_23_051455_create_finance_system_transaction_table.php
@@ -15,10 +15,13 @@ class CreateFinanceSystemTransactionTable extends Migration
     {
         Schema::create('finance_system_transaction', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('payed_to');
+            $table->unsignedBigInteger('payed_by');
+            $table->unsignedBigInteger('payment_info_id');
+            $table->double('amount_payed');
             $table->foreign('payed_to')->references('id')->on('finance_system_user');
             $table->foreign('payed_by')->references('id')->on('finance_system_student');
             $table->foreign('payment_info_id')->references('id')->on('finance_system_payment_info');
-            $table->double('amount_payed');
             $table->timestamps();
         });
     }
